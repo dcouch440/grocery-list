c9502bcb2f42adcb20224082184d440b180337be
dcouch440 <dcouch440@gmail.com>
2021-01-19 13:01:03 -0800

    add form and label for user input
    Co-Authored by Mathew Foley<mattfoley0375@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+/*
+
+  - create a form to get user input they need at the store
+  - take inputs and put them into an array
+  - display the list in alphebetical order
+  - create a new array with each entry upper case
+  - then display the items inside a <ul></ul>
+
+*/
+
